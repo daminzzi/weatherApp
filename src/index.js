2f76c2379f01668055b993f223104e934157d56a
dudghyoungho <syh3291@sogang.ac.kr>
2024-11-02 20:22:27 +0900

    modified:   dist/index.html 	new file:   src/components/timeComponent.js 	new file:   src/components/weatherComponent.js 	modified:   src/index.js 	modified:   src/style.css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const appDiv = document.getElementById('app'); 
+import { createTimeComponent } from "./components/timeComponent";
+import { createWeatherComponent } from "./components/weatherComponent";
+import { config } from "../apikeys";
+import "./style.css";
 
-  const title = document.createElement('h1');
-  title.textContent = '현재 날씨와 시간';
+document.addEventListener("DOMContentLoaded", () => {
+  const appDiv = document.getElementById("app");
+
+  const title = document.createElement("h1");
+  title.textContent = "현재 날씨와 시간";
   appDiv.appendChild(title);
 
-  const weatherInfo = document.createElement('div');
-  
+  const weatherInfo = document.createElement("div");
+
   // TODO: 시간과 날씨 정보를 컴포넌트로 생성하여 weatherInfo의 child로 추가.
-  
+  const timeComponent = createTimeComponent();
+  weatherInfo.appendChild(timeComponent);
+
+  const weatherComponent = createWeatherComponent(config.apikey);
+  weatherInfo.appendChild(weatherComponent);
   appDiv.appendChild(weatherInfo);
-})
+});
